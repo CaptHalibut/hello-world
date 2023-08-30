4bce5b8875c4651b29361ff914d6fc0bc7bf5a3c
mstephb <mstephb@mac.com>
2023-08-30 16:09:04 -0400

    Added main loop

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ WIN_LENGTH = 800
 WIN_HEIGHT = 600
 
 pg.init()
+
+screen = pg.display.set_mode((WIN_LENGTH, WIN_HEIGHT))
+screen.fill((0, 0, 0))
+
+pg.display.set_caption("Platformer")
+
+while True:
+
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            exit()
+
+    pg.display.update()
