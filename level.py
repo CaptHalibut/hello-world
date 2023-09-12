d31d5a2b7bd3d6d9ad3a1bb9c14cedf3a6a9e7eb
mstephb <mstephb@mac.com>
2023-09-12 12:40:54 -0400

    Platform and Level

--- level.py
+++ level.py
@@ -18,9 +18,10 @@ class Level(object):
         self.platform_list.draw(screen)
 
     def shift_world(self, shift_x):
-        self.world_shift += shift_x
-        for platform in self.platform_list:
-            platform.rect.x += shift_x
+        if self.level_boundaryL > self.world_shift and self.world_shift < self.level_boundaryR:
+            self.world_shift += shift_x
+            for platform in self.platform_list:
+                platform.rect.x += shift_x
       
         
 #First Level Platforms and Crates
@@ -30,7 +31,8 @@ class Level_01 (Level):
     def __init__(self, player): 
         Level.__init__(self, player)
 
-        self.level_boundary = -1000
+        self.level_boundaryL = 0
+        self.level_bouundaryR = 800
 
         # level_platforms = [((SCREEN_HEIGHT / 2, SCREEN_WIDTH / 2), SMALL_PLATFORM_SIZE, BROWN), ((500, 200), LARGE_PLATFORM_SIZE, GREEN_GRASS), ((1000, 600), THICK_PLATFORM_SIZE, BROWN)]
         
